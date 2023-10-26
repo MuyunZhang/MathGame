0305ce43171cc3979fb21f4b68388616b7fd5509
Muyun Zhang <muyunz@nycstudents.net>
2023-10-25 23:29:18 -0400

    k

--- src/MathGame.java
+++ src/MathGame.java
@@ -4,6 +4,8 @@ public class MathGame {
 
     private Player player1;
     private Player player2;
+
+    private Player player3;
     private Player currentPlayer;
     private Player winner;
     private boolean gameOver;
@@ -13,6 +15,7 @@ public class MathGame {
     public MathGame(Player player1, Player player2, Scanner scanner) {
         this.player1 = player1;
         this.player2 = player2;
+        this.player3 = player3;
         this.scanner = scanner;
         currentPlayer = null; // will get assigned at start of game
         winner = null; // will get assigned when a Player wins
@@ -37,10 +40,22 @@ public class MathGame {
                 System.out.println("Correct!");
                 currentPlayer.incrementScore();  // this increments the currentPlayer's score
                 swapPlayers();  // this helper method (shown below) sets currentPlayer to the other Player
-            } else {
+            }
+            else {
+                swapPlayers();
+                printGameState();   // this helper method (shown below) prints the state of the Game
+                System.out.println("Current player: " + currentPlayer.getName());
+                correct = askQuestion();  // this helper method (shown below) asks a question and returns T or F
+                if (correct) {
+                    System.out.println("Correct!");
+                    currentPlayer.incrementScore();  // this increments the currentPlayer's score
+                    swapPlayers();
+                }
+                else{
                 System.out.println("INCORRECT!");
                 gameOver = true;
                 determineWinner();
+                }
             }
         }
     }
@@ -51,6 +66,7 @@ public class MathGame {
         System.out.println("Current Scores:");
         System.out.println(player1.getName() + ": " + player1.getScore());
         System.out.println(player2.getName() + ": " + player2.getScore());
+        System.out.println(player3.getName() + ": " + player3.getScore());
         System.out.println("--------------------------------------");
     }
 
@@ -58,6 +74,7 @@ public class MathGame {
     public void resetGame() {
         player1.reset(); // this method resets the player
         player2.reset();
+        player3.reset();
         gameOver = false;
         currentPlayer = null;
         winner = null;
@@ -67,11 +84,15 @@ public class MathGame {
 
     // randomly chooses one of the Player objects to be the currentPlayer
     private void chooseStartingPlayer() {
-        int randNum = (int) (Math.random() * 2) + 1;
+        int randNum = (int) (Math.random() * 3) + 1;
         if (randNum == 1) {
             currentPlayer = player1;
-        } else {
+        }
+        else if(randNum == 2) {
             currentPlayer = player2;
+        }
+        else{
+            currentPlayer = player3;
         }
     }
 
@@ -114,7 +135,11 @@ public class MathGame {
     private void swapPlayers() {
         if (currentPlayer == player1) {
             currentPlayer = player2;
-        } else {
+        }
+        else if( currentPlayer == player2){
+            currentPlayer = player3;
+        }
+        else{
             currentPlayer = player1;
         }
     }
@@ -123,8 +148,12 @@ public class MathGame {
     private void determineWinner() {
         if (currentPlayer == player1) {
             winner = player2;
-        } else {
+        }
+        else if(currentPlayer == player2){
             winner = player1;
+        }
+        else{
+            winner = player3;
         }
     }
 }
